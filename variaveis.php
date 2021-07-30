aebd83b4c99fb93c65a046ddd0374f65e0173fe9
herculanosilva <herculano.silva@comparedistribuidora.com.br>
2021-07-30 10:46:46 -0300

    Variaveis

---
+++ variaveis.php
@@ -0,0 +1,22 @@
+Variáveis ​​PHP
+
+Variáveis ​​são "contêineres" para armazenar informações.
+
+Criação (declaração) de variáveis ​​PHP
+
+No PHP, uma variável começa com o sinal $, seguido pelo nome da variável:
+
+<?php
+    $txt = "Olá Mundo!";
+    $x = 5;
+    $y = 10.5;
+?>
+
+Regras para variáveis ​​PHP:
+
+    Uma variável começa com o sinal $, seguido pelo nome da variável
+    Um nome de variável deve começar com uma letra ou o caractere de sublinhado
+    O nome de uma variável não pode começar com um número
+    Um nome de variável pode conter apenas caracteres alfanuméricos e sublinhados (A-z, 0-9 e _)
+    Os nomes das variáveis ​​diferenciam maiúsculas de minúsculas ($variavel e $VARIAVEL são duas variáveis ​​diferentes)
+    

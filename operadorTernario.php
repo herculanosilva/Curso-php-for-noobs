c436c1acebc1f78aab83c428d41c174bf31b3d15
herculanosilva <herculano.silva@comparedistribuidora.com.br>
2021-08-19 10:13:52 -0300

    Operador Ternario

---
+++ operadorTernario.php
@@ -0,0 +1,19 @@
+<h2>Operador ternário</h2>
+
+<p>Outra estrutura de condição é o operador ternário, o qual também é chamado de if ternário. Em suma, ele é uma versão compacta do if.</p>
+
+<h3>Sintaxe</h3>
+
+<p>condição ? codigoUm : codigoDois;</p>
+
+
+<?php
+
+$idade = 18;
+
+$x = $idade >= 18 ? "É maior de idade" : "É menor de idade";
+
+echo $x;
+
+?>
+

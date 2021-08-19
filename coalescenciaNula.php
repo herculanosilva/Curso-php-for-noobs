d67e9612e89a706004f005b87c8cba1a03c0cb84
herculanosilva <herculano.silva@comparedistribuidora.com.br>
2021-08-19 11:53:26 -0300

    <?php

---
+++ coalescenciaNula.php
@@ -0,0 +1,5 @@
+<h2>Operador de coalescência nula</h2>
+
+<p>O operador de coalescência nula (??) foi adicionado como um truque sintático para o caso trivial de precisar usar um ternário em conjunto com a função isset().</p>
+
+<p> Ele retorna o primeiro operando se este existir e não for null; caso contrário retorna o segundo operando.</p>
